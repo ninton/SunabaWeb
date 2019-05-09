cb2352b900ea63b3e9a852c1d9becbc022ab4b51
aoki.makoto <aoki_makoto@ninton.co.jp>
2019-05-09 19:16:00 +0900

    Sunaba.readInstruction

--- test/sunaba/Sunaba.ts
+++ test/sunaba/Sunaba.ts
@@ -9,6 +9,20 @@ suite('sunaba.Compiler', () => {
     teardown(() => {
     });
 
+    test('readInstruction', () => {
+        const table = [
+            'i', 'add', 'sub', 'mul', 'div', 'lt', 'le', 'eq', 'ne',
+            'ld', 'st', 'fld', 'fst', 'j', 'bz', 'call', 'ret', 'pop'
+        ];
+
+        table.forEach((instruction) => {
+            assert.equal(instruction, Sunaba.readInstruction(instruction));
+        });
+
+        assert.equal("", Sunaba.readInstruction("hoge"));
+
+    });
+
     test('readNumber', () => {
         assert.equal( 1234, Sunaba.readNumber(" 1234 " , 1, 4));
         assert.equal(-1234, Sunaba.readNumber(" -1234 ", 1, 5));
