aeb3f260b33baec7dcac9307689e7f7ccc9f2449
aoki.makoto <aoki_makoto@ninton.co.jp>
2019-05-16 18:47:40 +0900

    eslint

--- src/sunaba/NodeType.ts
+++ src/sunaba/NodeType.ts
@@ -1,37 +1,39 @@
-export enum TermType {
-    TERM_EXPRESSION,
-	TERM_NUMBER,
-	TERM_FUNCTION,
-	TERM_ARRAY_ELEMENT,
-	TERM_VARIABLE,
-	TERM_OUT,
+/* eslint no-unused-vars: 0 */
 
-	TERM_UNKNOWN = 99   
+export enum TermType {
+  TERM_EXPRESSION,
+  TERM_NUMBER,
+  TERM_FUNCTION,
+  TERM_ARRAY_ELEMENT,
+  TERM_VARIABLE,
+  TERM_OUT,
+
+  TERM_UNKNOWN = 99
 }
 
 export enum StatementType {
-	STATEMENT_WHILE_OR_IF,
-	STATEMENT_DEF,
-	STATEMENT_CONST,
-	STATEMENT_FUNCTION,
-    STATEMENT_SUBSTITUTION,
-    
-	STATEMENT_UNKNOWN = 99
+  STATEMENT_WHILE_OR_IF,
+  STATEMENT_DEF,
+  STATEMENT_CONST,
+  STATEMENT_FUNCTION,
+  STATEMENT_SUBSTITUTION,
+
+  STATEMENT_UNKNOWN = 99
 }
 
 export enum NodeType {
-	// test/fixture/??_vmcode.json に記述しているので、値を固定した
-	NODE_PROGRAM                = 0,
-	NODE_WHILE_STATEMENT        = 1,
-	NODE_IF_STATEMENT           = 2,
-	NODE_SUBSTITUTION_STATEMENT = 3,
-	NODE_FUNCTION_DEFINITION    = 4,
-	NODE_EXPRESSION             = 5,
-	NODE_VARIABLE               = 6,
-	NODE_NUMBER                 = 7,
-	NODE_OUT                    = 8,
-	NODE_ARRAY_ELEMENT          = 9,
-	NODE_FUNCTION               = 10,
+  // test/fixture/??_vmcode.json に記述しているので、値を固定した
+  NODE_PROGRAM        			= 0,
+  NODE_WHILE_STATEMENT    		= 1,
+  NODE_IF_STATEMENT       		= 2,
+  NODE_SUBSTITUTION_STATEMENT 	= 3,
+  NODE_FUNCTION_DEFINITION  	= 4,
+  NODE_EXPRESSION       		= 5,
+  NODE_VARIABLE         		= 6,
+  NODE_NUMBER         			= 7,
+  NODE_OUT          			= 8,
+  NODE_ARRAY_ELEMENT      		= 9,
+  NODE_FUNCTION         		= 10,
 
-	NODE_UNKNOWN                = 99
+  NODE_UNKNOWN = 99
 }
