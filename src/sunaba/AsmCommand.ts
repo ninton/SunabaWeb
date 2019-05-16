25b41058728d76c1093c1c852e9b4e400ccdb326
aoki.makoto <aoki_makoto@ninton.co.jp>
2019-05-16 18:50:19 +0900

    eslint

--- src/sunaba/AsmCommand.ts
+++ src/sunaba/AsmCommand.ts
@@ -1,13 +1,13 @@
 export default class AsmCommand {
-    label:     string;
-    name:      string;
-    imm:       number|string = 0;
-    comment:   string = '';
+  label:    string;
+  name:     string;
+  imm:      number|string = 0;
+  comment:  string        = '';
 
-    constructor(label:string = '', name:string = '', imm:number|string = 0, comment:string = '') {
-        this.label   = label;
-        this.name    = name;
-        this.imm     = imm;
-        this.comment = comment;
-    }
+  constructor(label:string = '', name:string = '', imm:number|string = 0, comment:string = '') {
+    this.label    = label;
+    this.name     = name;
+    this.imm      = imm;
+    this.comment  = comment;
+  }
 }
