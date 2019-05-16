b2fa29eca11d79ce9367e1af2dbdcb32cd809475
aoki.makoto <aoki_makoto@ninton.co.jp>
2019-05-16 18:51:57 +0900

    eslint

--- src/sunaba/VmCommand.ts
+++ src/sunaba/VmCommand.ts
@@ -1,13 +1,13 @@
 export default class VmCommand {
-    label:     string = '';
-    name:      string;
-    imm:       number = 0;
-    comment:   string = '';
+  label:    string = '';
+  name:     string;
+  imm:      number = 0;
+  comment:  string = '';
 
-    constructor(label:string, name:string, imm:number = 0, comment:string = '') {
-        this.label   = label;
-        this.name    = name;
-        this.imm     = imm;
-        this.comment = comment;
-    }
+  constructor(label:string, name:string, imm:number = 0, comment:string = '') {
+    this.label    = label;
+    this.name     = name;
+    this.imm      = imm;
+    this.comment  = comment;
+  }
 }
