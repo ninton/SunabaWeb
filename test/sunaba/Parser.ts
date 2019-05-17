42ec75efb9ccc4aa6b505d7611863c52bbee4cb4
aoki.makoto <aoki_makoto@ninton.co.jp>
2019-05-17 10:58:38 +0900

    eslint

--- test/sunaba/Parser.ts
+++ test/sunaba/Parser.ts
@@ -1,43 +1,48 @@
-import assert = require("assert");
-import fs = require("fs");
-import Sunaba from "../../src/sunaba/Sunaba";
-import TokenType from "../../src/sunaba/TokenType";
-import Token from "../../src/sunaba/Token";
-import Parser from "../../src/sunaba/Parser";
+/* eslint-env mocha */
+import Sunaba from '../../src/sunaba/Sunaba';
+import TokenType from '../../src/sunaba/TokenType';
+import Parser from '../../src/sunaba/Parser';
+
+/* eslint no-unused-vars: 0 */
+import Token from '../../src/sunaba/Token';
+
+import assert = require('assert');
+import fs = require('fs');
 
 suite('sunaba.Parser', () => {
-    setup(() => {
-    });
+  setup(() => {
+  });
 
-    teardown(() => {
-    });
+  teardown(() => {
+  });
 
-    test('parseProgram　#1', () => {
-        const tokens = [
-            {type: TokenType.TOKEN_NAME         , line: 1, string: "memory"                },
-            {type: TokenType.TOKEN_INDEX_BEGIN  , line: 1, string: "["                     },
-            {type: TokenType.TOKEN_NUMBER       , line: 1, string: "65050" , number: 65050 },
-            {type: TokenType.TOKEN_INDEX_END    , line: 1, string: "]"                     },
-            {type: TokenType.TOKEN_SUBSTITUTION , line: 1, string: "→"                    },
-            {type: TokenType.TOKEN_NUMBER       , line: 1, string: "999999", number: 999999},
-            {type: TokenType.TOKEN_STATEMENT_END, line: 1, string: "行末"                  },
-            {type: TokenType.TOKEN_END          , line: 1                                  }
-        ];
-        const parser = new Parser(<Array<Token>>tokens, Sunaba.locales.japanese);
-        const root = parser.parseProgram();
-        //console.log(JSON.stringify(root, null, 4));
+  test('parseProgram　#1', () => {
+    /* eslint object-curly-newline: 0 */
+    const tokens = [
+      { type: TokenType.TOKEN_NAME,           line: 1,  string: 'memory'                  },
+      { type: TokenType.TOKEN_INDEX_BEGIN,    line: 1,  string: '['                       },
+      { type: TokenType.TOKEN_NUMBER,         line: 1,  string: '65050',  number: 65050   },
+      { type: TokenType.TOKEN_INDEX_END,      line: 1,  string: ']'                       },
+      { type: TokenType.TOKEN_SUBSTITUTION,   line: 1,  string: '→'                      },
+      { type: TokenType.TOKEN_NUMBER,         line: 1,  string: '999999', number: 999999  },
+      { type: TokenType.TOKEN_STATEMENT_END,  line: 1,  string: '行末'                    },
+      { type: TokenType.TOKEN_END,            line: 1                                     },
+    ];
+    const parser = new Parser(<Array<Token>>tokens, Sunaba.locales.japanese);
+    const root = parser.parseProgram();
+    // console.log(JSON.stringify(root, null, 4));
 
-        const expected = JSON.parse(fs.readFileSync('test/fixture/04_node.json').toString());
-        assert.deepEqual(expected, root);
-    });
+    const expected = JSON.parse(fs.readFileSync('test/fixture/04_node.json').toString());
+    assert.deepEqual(expected, root);
+  });
 
-    test('parseProgram #2', () => {
-        const tokens = JSON.parse(fs.readFileSync('test/fixture/06_token.json').toString());
-        const parser = new Parser(tokens, Sunaba.locales.japanese);
-        const root = parser.parseProgram();
-        //console.log(JSON.stringify(root, null, 4));
+  test('parseProgram #2', () => {
+    const tokens:Array<Token> = JSON.parse(fs.readFileSync('test/fixture/06_token.json').toString());
+    const parser = new Parser(tokens, Sunaba.locales.japanese);
+    const root = parser.parseProgram();
+    // console.log(JSON.stringify(root, null, 4));
 
-        const expected = JSON.parse(fs.readFileSync('test/fixture/06_node.json').toString());
-        assert.deepEqual(expected, root);
-    });
+    const expected = JSON.parse(fs.readFileSync('test/fixture/06_node.json').toString());
+    assert.deepEqual(expected, root);
+  });
 });
