4740b03546116cc48c0e52f9a75c68f67245a9a6
aoki.makoto <aoki_makoto@ninton.co.jp>
2019-05-17 11:05:28 +0900

    eslint

--- test/sunaba/MachineMessage.ts
+++ test/sunaba/MachineMessage.ts
@@ -1,79 +1,79 @@
-import assert = require("assert");
-import Machine from "../../src/sunaba/Machine";
+/* eslint-env mocha */
+import Machine from '../../src/sunaba/Machine';
+
+import assert = require('assert');
 
 suite('sunaba.MachineMessage', () => {
-    let machine:Machine;
+  let machine:Machine;
 
-    setup(() => {
-        machine = new Machine();
+  setup(() => {
+    machine = new Machine();
+  });
+
+  teardown(() => {
+  });
+
+  test('start program', () => {
+    const expected = [
+      'プログラムを起動',
+      'プログラムが最後まで実行された。',
+    ];
+
+    const program = [
+      {
+        name: 'i',
+        imm: 0,
+      },
+      {
+        name: 'i',
+        imm: 999999,
+      },
+      {
+        name: 'st',
+        imm: 60010,
+      },
+    ];
+
+    const mesgArr:Array<string> = [];
+    machine.setMessageHandler((mesg:string) => {
+      mesgArr.push(mesg);
     });
 
-    teardown(() => {
+    machine.loadProgram(program);
+    assert.deepEqual(['プログラムを起動'], mesgArr);
+
+    machine.step();
+    machine.step();
+    machine.step();
+
+    assert.deepEqual(expected, mesgArr);
+  });
+
+  test('0 div', () => {
+    const program = [
+      {
+        name: 'i',
+        imm: 6,
+      },
+      {
+        name: 'i',
+        imm: 0,
+      },
+      {
+        name: 'div',
+      },
+    ];
+
+    const mesgArr:Array<string> = [];
+    machine.setMessageHandler((mesg:string) => {
+      mesgArr.push(mesg);
     });
 
-    test('start program', () => {
-        const expected = [
-            "プログラムを起動",
-            "プログラムが最後まで実行された。"
-        ];
+    machine.loadProgram(program);
+    machine.step();
+    machine.step();
+    machine.step();
 
-        const program = [
-            {
-                name: 'i',
-                imm: 0
-            },
-            {
-                name: 'i',
-                imm: 999999
-            },
-            {
-                name: 'st',
-                imm: 60010
-            }
-        ];
-
-        const mesgArr:Array<string> = [];
-        machine.setMessageHandler((mesg:string) => {
-            mesgArr.push(mesg);
-        });
-
-        machine.loadProgram(program);
-        assert.deepEqual(["プログラムを起動"], mesgArr);
-
-        machine.step();
-        machine.step();
-        machine.step();
-
-        assert.deepEqual(expected, mesgArr);
-    });
-
-    test('0 div', () => {
-        const program = [
-            {
-                name: 'i',
-                imm: 6
-            },
-            {
-                name: 'i',
-                imm: 0
-            },
-            {
-                name: 'div'
-            }
-        ];
-
-        const mesgArr:Array<string> = [];
-        machine.setMessageHandler((mesg:string) => {
-            mesgArr.push(mesg);
-        });
-
-        machine.loadProgram(program);
-        machine.step();
-        machine.step();
-        machine.step();
-
-        assert.ok(mesgArr[1].indexOf('E910') >= 0);
-    });
-
-
+    assert.ok(mesgArr[1].indexOf('E910') >= 0);
+  });
 });
