abcf9421689c1650d5adeaf42220452d93c85ea4
aoki.makoto <aoki_makoto@ninton.co.jp>
2019-05-17 10:08:43 +0900

    eslint

--- test/sunaba/Assembler.ts
+++ test/sunaba/Assembler.ts
@@ -1,23 +1,25 @@
-import assert = require("assert");
-import fs = require("fs");
-import Assembler from "../../src/sunaba/Assembler";
+/* eslint-env mocha */
+import Assembler from '../../src/sunaba/Assembler';
+
+import assert = require('assert');
+import fs = require('fs');
 
 suite('sunaba.Assembler', () => {
-    let assembler:Assembler;
+  let assembler:Assembler;
 
-    setup(() => {
-        assembler = new Assembler();
-    });
+  setup(() => {
+    assembler = new Assembler();
+  });
 
-    teardown(() => {
-    });
+  teardown(() => {
+  });
 
-    test('Assember', () => {
-        const expected = JSON.parse(fs.readFileSync('test/fixture/04_vmcode.json').toString());
+  test('Assember', () => {
+    const expected = JSON.parse(fs.readFileSync('test/fixture/04_vmcode.json').toString());
 
-        const asmCmds = JSON.parse(fs.readFileSync('test/fixture/04_assemble.json').toString());
-        const vmCmds = assembler.assemble(asmCmds);
+    const asmCmds = JSON.parse(fs.readFileSync('test/fixture/04_assemble.json').toString());
+    const vmCmds = assembler.assemble(asmCmds);
 
-        assert.deepEqual(expected, vmCmds);
-    });
+    assert.deepEqual(expected, vmCmds);
+  });
 });
